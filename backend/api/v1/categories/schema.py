909fce5d81de9e165bdf0f0038f7eb05ddc1e53f
Misha <majikx.prod0@gmail.com>
2023-07-30 09:08:13 +0500

    change: schemes for categories

--- backend/api/v1/categories/schema.py
+++ backend/api/v1/categories/schema.py
@@ -6,23 +6,17 @@ from apps.categories.models import Category
 from api.v1.users.schema import UserOut
 
 
-class CategoryOutF(Schema):
-    id: int
+class CategoryOut(ModelSchema):
+    class Config:
+        model = Category
+        model_fields = '__all__'
 
 
-class CategoryOut(Schema):
-    id: int
-    title: str
-    description: Optional[str] = None
-    user: UserOut
-    color: str
-
-
-class CategoryIn(Schema):
-    title: str
-    description: Optional[str] = None
-    user: UserOut
-    color: Optional[str] = '2196F3'
+class CategoryIn(ModelSchema):
+    class Config:
+        model = Category
+        model_fields = '__all__'
+        model_exclude = ('id',)
 
 
 class NotFoundSchema(Schema):
