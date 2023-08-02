336c73e508db82f5f9d41f0dfd9964e72a309bdf
MAGICX <majikx.prod0@gmail.com>
2023-08-02 15:00:49 +0500

    add: django-cros-headers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,12 +57,14 @@ INSTALLED_APPS = [
 
     'ninja_extra',
     'ninja_jwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
