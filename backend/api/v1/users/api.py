eb3e9d2782cbcb45f7ea6dbcf2db897ed99da45a
MAGICX <majikx.prod0@gmail.com>
2023-08-02 23:02:54 +0500

    add: login and registration

--- backend/api/v1/users/api.py
+++ backend/api/v1/users/api.py
@@ -1,22 +1,40 @@
 from typing import List
 
+from django.contrib.auth import login, authenticate
 from ninja import Router
 from apps.users.models import User
-from api.v1.users.schema import UserOut
+from api.v1.users.schema import UserOut, NotFoundSchema, UserRegistrationInput, UserLoginInput, UserRegistrationAndLoginOutput
 
 router = Router()
 
 
-@router.get("/", response=List[UserOut])
+@router.get('/', response=List[UserOut])
 async def list_users(request) -> list:
     return [task async for task in User.objects.all()]
-    # qs = User.objects.exclude(username='admin')
-    # return qs
 
 
-@router.get("/{user_id}", response=UserOut)
+@router.get('/{user_id}', response={200: UserOut, 404: NotFoundSchema})
 async def get_user(request, user_id: int) -> dict:
     try:
-        return await User.objects.aget(id=user_id)
+        return 200, await User.objects.aget(id=user_id)
     except User.DoesNotExist as e:
-        return {'message': 'User does not exist'}
+        return 404, {'message': 'User does not exist'}
+
+
+@router.post('/register/', response={200: UserRegistrationAndLoginOutput, 404: NotFoundSchema}, auth=None)
+def register_user(request, payload: UserRegistrationInput) -> dict:
+    if not User.objects.filter(email=payload.email).exists():
+        user = User.objects.create_user(email=payload.email, nickname=payload.nickname, password=payload.password)
+        if user:
+            login(request, user)
+            return 200, {'id': user.id}
+    return 404, {'message': 'Username already exists'}
+
+
+@router.post('/login/', response={200: UserRegistrationAndLoginOutput, 402: NotFoundSchema}, auth=None)
+def login_user(request, payload: UserLoginInput) -> dict:
+    user = authenticate(email=payload.email, password=payload.password)
+    if user:
+        login(request, user)
+        return 200, {'id': user.id}
+    return 404, {'message': 'Invalid credentials'}
--- backend/api/v1/users/schema.py
+++ backend/api/v1/users/schema.py
@@ -1,7 +1,7 @@
-from datetime import datetime
 from typing import List
 
 from ninja import Schema, ModelSchema
+from pydantic import EmailStr
 
 from apps.users.models import User
 from api.v1.tasks.schema import TaskOut
@@ -15,6 +15,21 @@ class UserOut(ModelSchema):
     class Config:
         model = User
         model_fields = ('id', 'nickname', 'email', 'last_login', 'date_joined', 'is_active')
+
+
+class UserRegistrationInput(Schema):
+    email: EmailStr
+    nickname: str
+    password: str
+
+
+class UserRegistrationAndLoginOutput(Schema):
+    id: int
+
+
+class UserLoginInput(Schema):
+    email: EmailStr
+    password: str
 
 
 class NotFoundSchema(Schema):
