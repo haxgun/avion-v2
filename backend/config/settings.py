20fb42a2157d232944b2c20d4b2c42db7fc31afa
MAGICX <majikx.prod0@gmail.com>
2023-08-06 20:41:28 +0500

    pref: load dotenv file with pathlib

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,8 +18,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_file = os.path.join(BASE_DIR, '.env')
-if os.path.isfile(dotenv_file):
+dotenv_file = BASE_DIR / '.env'
+if dotenv_file.is_file():
     dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
