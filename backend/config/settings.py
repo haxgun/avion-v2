f954c00cf431576a35083befb476634395b69586
MAGICX <majikx.prod0@gmail.com>
2023-08-06 22:59:03 +0500

    build: add secrets in .env

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,11 +25,6 @@ if dotenv_file.is_file():
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
-
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
